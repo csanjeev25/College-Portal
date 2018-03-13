f44d38c1a2071b10319a637cea54c6a567a98bf6
csanjeev25 <sanjeevkumarchintakindi@gmail.com>
2018-03-14 00:05:35 +0530

    added home

--- main.py
+++ main.py
@@ -3,3 +3,48 @@ import sqllite3
 import smtplib
 from Crypto.Cipher import AES
 
+app=Flask(__name__)
+
+@app.route('/')
+@app.route('/<user_name>')
+def main(name=None):
+	return render_template('home.html',session=session,text=name)
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+global _id
